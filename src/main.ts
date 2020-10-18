8fe2a5ba31807c45dc51e386bcb28801728d2bbe
Daniel Perez <perez.daniel2105@gmail.com>
2020-10-18 10:39:09 -0400

    remove unnnesesary arguments

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,21 @@ import { isArray, isObject, isString, flatten, set } from './utils'
 
 export const lowerCaseValues = (payload: Object, keysToSkip: string[] = []) => {
   const flatSource = flatten(payload)
-  const transformedObj = lowerCase(payload, keysToSkip)
+  const transformedObj = lowerCase(payload)
   keysToSkip.forEach(path => set(transformedObj, path, flatSource[path]))
 
   return transformedObj
 }
 
-const lowerCase = (payload: Object, keysToSkip: string[] = []) =>
+const lowerCase = (payload: Object) =>
   Object.entries(payload).reduce((updated, [key, value]) => {
     if (isArray(value)) {
-      updated[key] = value.map(val => lowerCase(val, keysToSkip))
+      updated[key] = value.map(val => lowerCase(val))
       return updated
     }
 
     if (isObject(value)) {
-      updated[key] = lowerCase(value, keysToSkip)
+      updated[key] = lowerCase(value)
       return updated
     }
 
